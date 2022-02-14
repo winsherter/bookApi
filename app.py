6eee3c9aaade826fd9f82c5a4c1c67b9be8372f2
winsherter <gilbertwinsherter@gmail.com>
2022-02-14 07:42:18 +0000

    Second Deploy

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ class Livre(db.Model):
             "Editeur":self.editeur, 
             "Code Categorie":self.idcat, 
             "Date de Publication":self.datepub.strftime("%b %d, %Y") }
-db.create_all()
+
 ########################################################################################################################################
 #
 #                                        AFFICHER TOUT LES LIVRES
@@ -308,3 +308,7 @@ def badRequest(error):
         "Message":"Method Not Allowed",
         "Error":405
     }),405
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
